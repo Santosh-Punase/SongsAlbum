3fa65f5c024ebbfcf2766e84ac7f625b552c5942
santosh punase <santosh.punase@rario.com>
2025-02-12 12:43:55 +0530

    Read library url from env

--- music-home/vite.config.ts
+++ music-home/vite.config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "musicHome",
-      remotes: {
-        musicLibrary: "http://localhost:5173/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.LIBRARY_API_URL ?? 'http://localhost:5173'}`;
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "musicHome",
+        remotes: {
+          musicLibrary: `${API_URL}/assets/remoteEntry.js`,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  }
 });
