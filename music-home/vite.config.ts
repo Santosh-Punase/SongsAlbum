e7bdd75cc31b3d5d1c1d7e6ebaec22f10378c8f4
santosh punase <santosh.punase@rario.com>
2025-02-12 14:21:36 +0530

    env fix

--- music-home/vite.config.ts
+++ music-home/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const API_URL = `${env.LIBRARY_API_URL ?? 'http://localhost:5173'}`;
+export default defineConfig(() => {
+  const API_URL = `${process.env.LIBRARY_API_URL ?? 'http://localhost:5173'}`;
 
   return {
     plugins: [
